b89d2f12041f45c497759dd266a15da91bcdcff7
osvalB <oburastero@gmail.com>
2026-02-17 10:10:10 +0100

    remove extra file

--- appFiles/MoltenProt/test_supr.py
+++
@@ -1,9 +0,0 @@
-from main import  ManyDsfFitters
-
-file = "/home/os/Downloads/Experiment 12_02_2026-ramp-plate2.supr"
-
-fitters = ManyDsfFitters()
-
-fitters.add_experiment(file,"test1")
-
-fitters.set_signal(fitters.all_signals[0])
